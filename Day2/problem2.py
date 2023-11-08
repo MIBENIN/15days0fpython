e6da183743d7050cbc717fb1b23e2a46876b8339
Benin S <thorben21dash@gmail.com>
2023-11-08 07:12:25 +0530

    Problem 2 added

---
+++ Day2/problem2.py
@@ -0,0 +1,6 @@
+'''
+Create a program that takes a temperature in celcius and convert it to Kelvin
+'''
+Celsius = float(input("Enter the celcuis :"))
+Kelvin = Celsius + 273.15
+print("Kelvin :", Kelvin)
